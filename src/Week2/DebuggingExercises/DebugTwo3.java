3fd5c424f78ff8926b2e48fcb6ffdab259316cc3
LKWBrando <brandon.lum@my.jcu.edu.au>
2017-07-27 09:37:21 +0800

    Added Week2 debugging exercises

---
+++ src/Week2/DebuggingExercises/DebugTwo3.java
@@ -0,0 +1,16 @@
+package Week2.DebuggingExercises;
+
+public class DebugTwo3
+// Demonstrates remainder and output
+{
+   public static void main(String[] args)
+   {
+      int a = 99, b = 8, result;
+      long c = 7777777777777L;
+      result = a % b;
+      System.out.println("Divide " + a + " by " + b);
+      System.out.println("remainder is " + a);
+      System.out.print("c is a very large number: ");
+      System.out.println(c);
+    }
+}
---
+++ src/Week2/DebuggingExercises/DebugTwo4.java
@@ -0,0 +1,17 @@
+package Week2.DebuggingExercises;
+
+import javax.swing.JOptionPane;
+public class DebugTwo4
+{
+   public static void main(String[] args)
+   {
+      String costString;
+      double cost;
+      final double tax = 0.06;
+      costString = JOptionPane.showInputDialog(null, "Enter price of item you are buying","Purchases",
+         JOptionPane.INFORMATION_MESSAGE);
+      cost = Double.parseDouble(costString);
+      JOptionPane.showMessageDialog(null,"With " + tax * 100 +
+         "% tax,  purchase  is $" + (cost - cost * tax));
+   }
+}
